dfe0c61ec4c37d6ff0f0d995f0045724b1c4bdc7
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-10-30 21:25:59 +0100

    new: Commented out serde_derive crate which is currently unused

--- src/bin/sysapi.rs
+++ src/bin/sysapi.rs
@@ -14,7 +14,7 @@ extern crate mime;
 extern crate regex;
 extern crate serde;
 // #[macro_use]
-extern crate serde_derive;
+// extern crate serde_derive;
 extern crate serde_json;
 
 extern crate sysapi;
