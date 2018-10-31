53b77a9a681dc7d497aac643b5e199b470be8ee0
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-10-31 18:38:00 +0100

    new: Extended utils::produce_list(). Defined utils::list_attributes()

--- src/webapi/utils.rs
+++ src/webapi/utils.rs
@@ -4,9 +4,8 @@ use glob::glob;
 use api::SENTRY_PATH;
 
 
-/// Lists all available cells based on files found under Sentry dir:
-pub fn list_cells() -> Vec<String> {
-    let glob_pattern = format!("{}/*", SENTRY_PATH);
+/// Produce list of dirs/files matching given glob pattern:
+pub fn produce_list(glob_pattern: &String) -> Vec<String> {
     let mut list = vec!();
     for entry in glob(&glob_pattern).unwrap() {
         match entry {
@@ -28,6 +27,21 @@ pub fn list_cells() -> Vec<String> {
             },
         }
     }
-    debug!("list_cells(): Elements: {:?}", list);
+    debug!("produce_list(): Elements: {:?}", list);
     list
 }
+
+
+// Lists all cell attributes => /Shared/Prison/Sentry/CELLNAME/cell-attributes/*
+pub fn list_attributes(cell_name: &String) -> Vec<String> {
+    let glob_pattern = format!("{}/{}/cell-attributes/*", cell_name, SENTRY_PATH);
+    produce_list(&glob_pattern)
+}
+
+
+/// Lists all available cells based on files found under Sentry dir:
+pub fn list_cells() -> Vec<String> {
+    let glob_pattern = format!("{}/*", SENTRY_PATH);
+    produce_list(&glob_pattern)
+}
+
