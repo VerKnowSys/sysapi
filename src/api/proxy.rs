f09e9b65b4bd96d0f187d1da994a0f72de2ee4a5
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-01 04:13:42 +0100

    new: Extended Proxy structure with from_ipv4 and to_ipv4 fields. Also all made optional.

--- src/api/proxy.rs
+++ src/api/proxy.rs
@@ -1,11 +1,21 @@
+use std::net::IpAddr;
 /// Web proxy wrapper:
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Proxy {
 
     /// Proxy from URL:
-    from: Option<String>,
+    pub from: Option<String>,
+
+    /// Resolved IPv4 from "from" URL:
+    pub from_ipv4: Option<IpAddr>,
 
     /// Proxy to URL:
-    to: Option<String>,
+    pub to: Option<String>,
+
+    /// Resolved IPv4 from "to" URL:
+    pub to_ipv4: Option<IpAddr>,
+
+}
+
 
 }
