01bc9cbe88d639791157516a9f404dba153fb25a
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-02 05:28:17 +0100

    imp: Applied common nesting formatting for router. Router is now generated admin-panel friendly too.

--- src/webapi/router.rs
+++ src/webapi/router.rs
@@ -22,68 +22,87 @@ pub fn router() -> Router {
 
         /* Dashboard */
 
-        route.get("/").to_file("web/dashboard.html");
+        //
+        // NOTE: using simplest yet most powerful admin template I've found:
+        // https://github.com/coreui/coreui-free-bootstrap-admin-template-ajax#versions
+        //
+        // NOTE: This is pure Ajax version that has to be installed manually, following steps::
+        //
+        // # make sure you have nodejs installed - version 10+ is latest LTS.
+        //
+        //      git clone https://github.com/coreui/coreui-free-bootstrap-admin-template-ajax.git
+        //      cd coreui-free-bootstrap-admin-template-ajax
+        //      npm install
+        //      npm run build
+        //      cp -R dist/* /your/dir/to/sysapi/web/
+        //
 
-        route.get("/css/*").to_dir(
-            FileOptions::new("web/static/css/")
+        route
+            .get("/")
+            .to_file("web/index.html");
+
+        route
+            .get("/*")
+                .to_dir(FileOptions::new("web/")
                 .with_gzip(true)
-                .build(),
+                .build()
         );
 
-        route.get("/js/*").to_dir(
-            FileOptions::new("web/static/js/")
-                .with_gzip(true)
-                .build(),
-        );
-
-        route.get("/static/*").to_dir(
-            FileOptions::new("web/static/")
-                .with_gzip(false)
-                .build(),
+        route
+            .get("/static/*")
+            .to_dir(FileOptions::new("web/static/")
+            .with_gzip(false)
+            .build()
         );
 
 
         /* WebAPI: */
 
         // …/cells/list
-        route.associate(
-            &format!("{}list", CELLS_RESOURCE), |handler| {
-                handler.get().to(cells_get_handler);
-            });
+        route
+            .associate(
+                &format!("{}list", CELLS_RESOURCE), |handler| {
+                    handler.get().to(cells_get_handler);
+                });
 
         // …/cell/:cell
-        route.associate(
-            &format!("{}:cell", CELL_RESOURCE), |handler| {
-                handler.get().to(cell_get_handler);
-                handler.post().to(cell_post_handler);
-                handler.delete().to(cell_delete_handler);
-            });
+        route
+            .associate(
+                &format!("{}:cell", CELL_RESOURCE), |handler| {
+                    handler.get().to(cell_get_handler);
+                    handler.post().to(cell_post_handler);
+                    handler.delete().to(cell_delete_handler);
+                });
 
         // …/snapshot/list/:cell
-        route.associate(
-            &format!("{}list/:cell", SNAPSHOT_RESOURCE), |handler| {
-                handler.get().to(zfs_snapshot_list_handler);
-            });
+        route
+            .associate(
+                &format!("{}list/:cell", SNAPSHOT_RESOURCE), |handler| {
+                    handler.get().to(zfs_snapshot_list_handler);
+                });
 
         // …/snapshot/:cell/:snapshot
-        route.associate(
-            &format!("{}:cell/:snapshot", SNAPSHOT_RESOURCE), |handler| {
-                handler.get().to(zfs_snapshot_get_handler);
-                handler.post().to(zfs_snapshot_post_handler);
-                handler.delete().to(zfs_snapshot_delete_handler);
-            });
+        route
+            .associate(
+                &format!("{}:cell/:snapshot", SNAPSHOT_RESOURCE), |handler| {
+                    handler.get().to(zfs_snapshot_get_handler);
+                    handler.post().to(zfs_snapshot_post_handler);
+                    handler.delete().to(zfs_snapshot_delete_handler);
+                });
 
         // …/rollback/:cell/:snapshot
-        route.associate(
-            &format!("{}:cell/:snapshot", ROLLBACK_RESOURCE), |handler| {
-                handler.post().to(zfs_rollback_post_handler);
-            });
+        route
+            .associate(
+                &format!("{}:cell/:snapshot", ROLLBACK_RESOURCE), |handler| {
+                    handler.post().to(zfs_rollback_post_handler);
+                });
 
 
         // …/proxy/:cell/:from/:to
-        route.associate(
-            &format!("{}:cell/:from/:to", PROXY_RESOURCE), |handler| {
-                handler.post().to(web_proxy_post_handler);
-            });
+        route
+            .associate(
+                &format!("{}:cell/:from/:to", PROXY_RESOURCE), |handler| {
+                    handler.post().to(web_proxy_post_handler);
+                });
     })
 }
