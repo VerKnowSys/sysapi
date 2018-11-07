88ef194cad5b763b27169cadd0044c9f8553cadf
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-07 19:12:19 +0100

    new: Defined fill_list_of_datasets() for view utils

--- web/static/js/utils.js
+++ web/static/js/utils.js
@@ -49,3 +49,41 @@ function fill_list_of_snapshots() {
     }
   });
 }
+
+
+// Auto-Fill select with available Cell datasets
+function fill_list_of_datasets() {
+  var selected_cell_name = $("select.cell_name").val();
+  if (selected_cell_name != undefined && selected_cell_name != "") {
+    $.ajax({
+      type: "GET",
+      url: "/datasets/list/".concat(selected_cell_name),
+      dataType: "json",
+      contentType : "application/json",
+      success: function(data) {
+        for (var i = data.list.length - 1; i >= 0; i--) {
+          var cell = data.list[i];
+          $.ajax({
+            type: "GET",
+            url: "/datasets/".concat(cell.cell_name),
+            dataType: "json",
+            contentType : "application/json",
+            success: function(data) {
+              for (var i = data.list.length - 1; i >= 0; i--) {
+                var dataset_and_snapshot = data.list[i];
+                if (dataset_and_snapshot != undefined && dataset_and_snapshot != "") {
+                  $('select.datasets_names').append("<option>".concat(dataset_and_snapshot).concat("</option>"));
+                } else {
+                  $("select.datasets_names").addClass("is-invalid");
+                }
+              }
+            },
+            error: function(doc, err) {
+              $("select.datasets_names").addClass("is-invalid");
+            }
+          });
+        }
+      }
+    });
+  }
+}
