2e75cc05a0df292b6ec9adcd31af57ded3059891
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-07 18:17:07 +0100

    new: Implemented utils::fill_list_of_snapshots()

--- web/static/js/utils.js
+++ web/static/js/utils.js
@@ -14,3 +14,38 @@ function fill_list_of_cells() {
     }
   });
 }
+
+
+// Auto-Fill select with available Cell snapshots
+function fill_list_of_snapshots() {
+  $.ajax({
+    type: "GET",
+    url: "/cells/list",
+    dataType: "json",
+    contentType : "application/json",
+    success: function(data) {
+      for (var i = data.list.length - 1; i >= 0; i--) {
+        var cell = data.list[i];
+        $.ajax({
+          type: "GET",
+          url: "/snapshot/list/".concat(cell.cell_name),
+          dataType: "json",
+          contentType : "application/json",
+          success: function(data) {
+            for (var i = data.list.length - 1; i >= 0; i--) {
+              var snapshot = data.list[i];
+              if (snapshot != undefined && snapshot != "") {
+                $('select.snapshot_names').append("<option>".concat(snapshot.name).concat("</option>"));
+              } else {
+                $("select#snapshot_name").addClass("is-invalid");
+              }
+            }
+          },
+          error: function(doc, err) {
+            $("select#snapshot_name").addClass("is-invalid");
+          }
+        });
+      }
+    }
+  });
+}
