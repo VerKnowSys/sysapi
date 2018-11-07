fba313bf56b920ab14c574d9e5e6189941b8f225
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-07 18:18:10 +0100

    new: Use html select for snapshots list. Try invoking fill_list_of_snapshots() on keyup (temporary).

--- web/static/js/rollback.js
+++ web/static/js/rollback.js
@@ -1,7 +1,7 @@
 // Rollback to snapshot:
 function create_rollback() {
     var cell_name = $("select#cell_name").val();
-    var snapshot_name = $("input#snapshot_name").val();
+    var snapshot_name = $("select#snapshot_name").val();
     var dataset_path = $("input#snapshot_dataset_path").val();
     if (snapshot_name != undefined && cell_name != undefined && snapshot_name != "" && cell_name != "" && dataset_path != undefined && dataset_path != "") {
         var url = "/rollback/".concat(cell_name).concat("/").concat(snapshot_name);
@@ -13,12 +13,12 @@ function create_rollback() {
             contentType : "application/json",
             statusCode: {
               406: function() { // not allowed
-                $("input#snapshot_name").addClass("is-invalid");
+                $("select#snapshot_name").addClass("is-invalid");
               }
             },
             success: function(){
                 $("select#cell_name").removeClass("is-invalid");
-                $("input#snapshot_name").removeClass("is-invalid");
+                $("select#snapshot_name").removeClass("is-invalid");
                 $("div.valid-feedback").show();
             }
         });
@@ -29,7 +29,7 @@ function create_rollback() {
             $("select#cell_name").removeClass("is-invalid");
             $("select#cell_name").addClass("is-valid");
         }
-        $("input#snapshot_name").addClass("is-invalid");
+        $("select#snapshot_name").addClass("is-invalid");
     }
 }
 
@@ -37,4 +37,5 @@ function create_rollback() {
 $( document ).ready(function() {
   $('li.location_info').text("System Management Interface - New ZFS Rollback");
   fill_list_of_cells();
+  fill_list_of_snapshots();
 });
