8d46cd8d0e8477c5b1fe813147760dc5bbab50c0
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-08 12:42:37 +0100

    new: Render Snapshot list by cell_name

--- web/static/js/snapshots.js
+++ web/static/js/snapshots.js
@@ -6,42 +6,30 @@ function clean_snapshots() {
 
 
 function render_snapshots() {
-  $.ajax({
+  var cell_name = $("select#cell_name").val();
+  if (cell_name != null && cell_name != undefined && cell_name != "") {
+    $.ajax({
       type: "GET",
-      url: "/cells/list",
+      url: "/snapshot/list/".concat(cell_name),
       dataType: "json",
       contentType : "application/json",
-      success: function(data) {
-        console.log("Got: " + JSON.stringify(data.list.length) + " object cells.");
-        for (var i = data.list.length - 1; i >= 0; i--) {
-          var cell = data.list[i];
-          var cell_name = cell.name;
-          var url = "/snapshot/list/".concat(cell.name);
-          if (cell.name != undefined && cell.name != "") {
-            $.ajax({
-              type: "GET",
-              url: url,
-              dataType: "json",
-              contentType : "application/json",
-              success: function(snaps) {
-                console.log("Got: " + snaps.list.length + " snapshots.");
-                for (var j = snaps.list.length - 1; j >= 0; j--) {
-                  var dataset_and_snap = snaps.list[j];
-                  var snapshot_name = dataset_and_snap.split("@")[1];
-                  if (snapshot_name == "origin" || snapshot_name == "after_export")
-                    continue;
+      success: function(snaps) {
+        console.log("Got: " + snaps.list.length + " snapshots.");
+        for (var j = snaps.list.length - 1; j >= 0; j--) {
+          var dataset_and_snap = snaps.list[j];
+          var snapshot_name = dataset_and_snap.split("@")[1];
+          if (snapshot_name == "origin" || snapshot_name == "after_export")
+            continue;
 
-                  console.log("Got: '" + snapshot_name + "' snapshot name.");
-
-                  if (snapshot_name != undefined && snapshot_name != "") {
-                    $.ajax({
-                      type: "GET",
-                      url: "/snapshot/".concat(cell_name).concat("/").concat(snapshot_name),
-                      dataType: "json",
-                      contentType : "application/json",
-                      success: function(snapshot_obj) {
-                        console.log("SNAPSHOT_OBJECT: ".concat(JSON.stringify(snapshot_obj)));
-                        var snapshot_template = "\
+          console.log("Got: '" + dataset_and_snap + "' - 'dataset@snapshot'");
+          $.ajax({
+            type: "GET",
+            url: "/snapshot/".concat(cell_name).concat("/").concat(snapshot_name),
+            dataType: "json",
+            contentType : "application/json",
+            success: function(snapshot_obj) {
+              console.log("SNAPSHOT_OBJECT: " + JSON.stringify(snapshot_obj));
+              var snapshot_template = "\
 <tr class=\"delete_snapshot\" cell_name=\"__CELL_NAME__\" snapshot_name=\"__SNAPSHOT_NAME__\" dataset_path=\"__DATASET_PATH__\"> \
   <td>__CELL_NAME__</td> \
   <td>__SNAPSHOT_NAME__</td> \
@@ -49,23 +37,18 @@ function render_snapshots() {
   <td>__TIMESTAMP__</td> \
 </tr> \
 ";
-                        snapshot_template = snapshot_template.replace(/__SNAPSHOT_NAME__/g, snapshot_obj.name);
-                        snapshot_template = snapshot_template.replace(/__CELL_NAME__/g, snapshot_obj.cell_name);
-                        snapshot_template = snapshot_template.replace(/__DATASET_PATH__/g, snapshot_obj.dataset_path);
-                        snapshot_template = snapshot_template.replace(/__TIMESTAMP__/g, snapshot_obj.timestamp);
-                        console.log("Snapshot template: ".concat(snapshot_template));
-                        $("tbody.snapshots_list").append(snapshot_template);
-                      }
-                    });
-
-                  }
-                }
-              }
-            });
-          }
+              snapshot_template = snapshot_template.replace(/__SNAPSHOT_NAME__/g, snapshot_obj.name);
+              snapshot_template = snapshot_template.replace(/__CELL_NAME__/g, snapshot_obj.cell_name);
+              snapshot_template = snapshot_template.replace(/__DATASET_PATH__/g, snapshot_obj.dataset_path);
+              snapshot_template = snapshot_template.replace(/__TIMESTAMP__/g, snapshot_obj.timestamp);
+              console.log("Snapshot template: " + snapshot_template);
+              $("tbody.snapshots_list").append(snapshot_template);
+            }
+          });
         }
       }
     });
+  }
 }
 
 
