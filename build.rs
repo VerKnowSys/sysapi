eb66068388d075bb64da48ac346e42f2133d2877
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-11-22 01:34:52 +0100

    new: Added new build dependency: "cc".  Defined custom C/C++ library build and link

---
+++ build.rs
@@ -0,0 +1,34 @@
+// build.rs
+
+extern crate cc;
+
+
+fn main() {
+    cc::Build::new()
+        .include("/usr/include")
+        .file("../kvmpro/src/kvmpro.h")
+        .file("../kvmpro/src/kvm.cc")
+        .file("../kvmpro/src/procstat.cc")
+        .file("../kvmpro/src/utils.cc")
+        .pic(true)
+        .cpp(true)
+        .flag("-O3")
+        .flag("-fuse-ld=lld")
+        .flag("-fPIC")
+        .flag("-fPIE")
+        .flag("-ftrapv")
+        .flag("-mretpoline")
+        .flag("-fstack-protector")
+        .flag("-fstack-protector-strong")
+        .flag("-fstack-protector-all")
+        .flag("-fno-strict-overflow")
+        .flag("-Wformat")
+        .flag("-Wformat-security")
+        .flag("-Wl,-z,relro,-z,now")
+        .flag("-Wl,-z,retpolineplt")
+        .flag("-D_FORTIFY_SOURCE=2")
+        .static_flag(false)
+        .shared_flag(true)
+        .cpp_link_stdlib("c++")
+        .compile("libkvmpro.so");
+}
