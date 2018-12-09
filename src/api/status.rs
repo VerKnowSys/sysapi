39a8cdd6ca57c2e1bb2834152c23c5630b473ec7
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-10 00:38:55 +0100

    new: Implemented CellProcess structure. More in details.
    * CellProcesses can be deserialized using input from full list of processes function.
    * Implemented CellProcesses.
    * Implemented ToString and Default for CellProcesses. Return empty list for default()
    * Implemented ToString for CellProcess

--- src/api/status.rs
+++ src/api/status.rs
@@ -1,24 +1,81 @@
 
-/// Status of the cell representation:
+
+/// A single cell process status:
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct CellStatus {
+pub struct CellProcess {
 
-    /// Name of software bundle:
-    services: Option<Vec<String>>,
+    /// Process-IDentifier:
+    pid: Option<usize>,
 
-    /// Current CPU usage:
-    cpu: Option<usize>,
+    /// Parent Process-IDentifier:
+    ppid: Option<usize>,
 
-    /// Current RSS usage:
-    mem: Option<usize>,
+    /// Process-name:
+    name: Option<String>,
 
-    /// Current CPUTIME usage:
-    time: Option<usize>,
+    /// Process-command:
+    cmd: Option<String>,
 
-    /// Current WRITES usage:
-    io_writes: Option<usize>,
+    /// Process-RSS (Ressident Set Size - ressident memory allocated by the process):
+    rss: Option<usize>,
 
-    /// Current READS usage:
-    io_reads: Option<usize>,
+    /// Process-MRSS (Max Ressident Set Size - maximum amount of virtual memory process can allocate)
+    mrss: Option<usize>,
 
+    /// Process-run time:
+    runtime: Option<usize>,
+
+    /// Blocks written by Cell:
+    blk_in: Option<usize>,
+
+    /// Blocks read by Cell:
+    blk_out: Option<usize>,
+
+    /// Process threads count:
+    nthr: Option<usize>,
+
+    /// Process priority:
+    pri_level: Option<usize>,
+
+    /// Process stats including bound UDP or TCP ports/addresses and other info:
+    stat_info: Option<String>,
+
+}
+
+
+/// Status of all processes running in a cell:
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CellProcesses {
+
+    /// List of all processes
+    pub list: Vec<CellProcess>
+
+}
+
+
+/// An empty CellProcesses as default:
+impl Default for CellProcesses {
+    fn default() -> CellProcesses {
+        CellProcesses {
+            list: vec!()
+        }
+    }
+}
+
+
+/// Serialize to JSON on .to_string()
+impl ToString for CellProcess {
+    fn to_string(&self) -> String {
+        serde_json::to_string(&self)
+            .unwrap_or(String::from("{\"status\": \"SerializationFailure: CellProcess\"}"))
+    }
+}
+
+
+/// Serialize to JSON on .to_string()
+impl ToString for CellProcesses {
+    fn to_string(&self) -> String {
+        serde_json::to_string(&self.list)
+            .unwrap_or(String::from("{\"status\": \"SerializationFailure: CellProcesses\"}"))
+    }
 }
