605c02851a029ec7c173946fb30506d0f5d4caf1
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-10 20:27:31 +0100

    fix: Removed all bonus compiler options for build.rs

--- build.rs
+++ build.rs
@@ -3,25 +3,14 @@
 extern crate cc;
 
 
-// ??? NOTE: This process fails on svdOS builds older or than #131
-
-
 fn main() {
     cc::Build::new()
         .cpp(true)
         .pic(true)
         .warnings(true)
-        .flag("-Ofast")
+        .flag("-O2")
         .flag("-fPIE")
         .flag("-std=c++11")
-        .flag("-ftrapv")
-        .flag("-fstack-protector")
-        .flag("-fstack-protector-strong")
-        .flag("-fstack-protector-all")
-        .flag("-Wformat")
-        .flag("-Wformat-security")
-        .flag("-fno-strict-overflow")
-        .flag("-mretpoline")
         .cpp_set_stdlib("c++")
         .cpp_link_stdlib("c++")
         .file("lib/kvmpro/src/kvmpro.h")
