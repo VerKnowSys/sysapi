1c0c499b319db9bf1d0486015077d41c23d60c42
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-12 19:56:34 +0100

    new: Provided first example (more a test of shared object calls)

---
+++ src/examples/psuid65tojson.rs
@@ -0,0 +1,34 @@
+//! Example #01
+
+#![deny(
+    missing_docs,
+    unstable_features,
+    unsafe_code,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_import_braces,
+    unused_qualifications)]
+
+
+#[allow(unused_imports)]
+#[macro_use]
+extern crate log;
+
+#[allow(unused_imports)]
+#[macro_use]
+extern crate serde_derive;
+
+
+use sysapi::processes_of_uid;
+use sysapi::processes_of_uid_short;
+
+
+#[link(name = "kvmpro", kind = "dylib")]
+
+/// ProcessList for uid 65 into JSON
+pub fn main() {
+    println!("Process of UID 65 (LONG)  OUT: {:?}", processes_of_uid(65));
+    println!("Process of UID 65 (SHORT) OUT: {:?}", processes_of_uid_short(65));
+}
