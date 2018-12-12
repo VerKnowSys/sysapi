3e580fc4d1a334ca7511ec22bdee6d7d26a59d51
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-12 20:01:47 +0100

    new: Implemented two separate bench routines for each exported SO function.

---
+++ src/benches/kernel_kvm+procstat+uid65+sockstat.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate bencher;
+
+
+use bencher::Bencher;
+use self::common::utils::{processes_of_uid, processes_of_uid_short};
+
+
+fn bench_processes_of_uid_full(b: &mut Bencher) {
+    b.iter(|| processes_of_uid(65))
+}
+
+
+benchmark_group!(benches, bench_processes_of_uid_full, bench_processes_of_uid_short);
+benchmark_main!(benches);
