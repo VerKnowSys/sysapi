8055df65a35b16dcbaa1e87a05a48b7ebe9a20ad
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-12 20:00:33 +0100

    new: All constants are publically available in root library path.

--- src/lib.rs
+++ src/lib.rs
@@ -24,87 +24,101 @@ extern crate log;
 #[macro_use]
 extern crate serde_derive;
 
-    /// Project directory (for static files access for router):
-    pub const PROJECT_DIRECTORY: &str = "/Projects/sysapi";
 
-    /// Default log output file:
-    pub const DEFAULT_LOG_FILE: &str = "/var/log/sysapi.log";
+// Library constants, used by the project:
 
-    /// svdOS cell governor:
-    pub const GVR_BIN: &str = "/usr/bin/gvr";
+/// Absolute path to libkvmpro.so shared library installed on the production system:
+pub const DEFAULT_LIBKVMPRO_SHARED: &str = "/usr/lib/libkvmpro.so";
 
-    /// ZFS utility:
-    pub const ZFS_BIN: &str = "/sbin/zfs";
+/// Project directory (for static files access for router):
+pub const PROJECT_DIRECTORY: &str = "/Projects/sysapi";
 
-    /// BSD jail utility:
-    pub const JAIL_BIN: &str = "/usr/sbin/jail";
+/// Default log output file:
+pub const DEFAULT_LOG_FILE: &str = "/var/log/sysapi.log";
 
-    /// BSD jail-exec utility:
-    pub const JEXEC_BIN: &str = "/usr/sbin/jexec";
+/// svdOS cell governor:
+pub const GVR_BIN: &str = "/usr/bin/gvr";
 
-    /// Default username (jail user):
-    pub const CELL_USERNAME: &str = "worker";
+/// ZFS utility:
+pub const ZFS_BIN: &str = "/sbin/zfs";
 
-    /// Default local DNS server address:
-    pub const DEFAULT_DNS: &str = "172.16.3.1";
+/// BSD jail utility:
+pub const JAIL_BIN: &str = "/usr/sbin/jail";
 
-    /// Default listen address to listen on:
-    pub const DEFAULT_ADDRESS: &str = "172.16.3.1:80";
+/// BSD jail-exec utility:
+pub const JEXEC_BIN: &str = "/usr/sbin/jexec";
 
-    /// Default path to Prison root dir:
-    pub const PRISON_PATH: &str = "/Shared/Prison";
+/// Default username (jail user):
+pub const CELL_USERNAME: &str = "worker";
 
-    /// Default path to cells data dirs:
-    pub const CELLS_PATH: &str = "/Shared/Prison/Cells";
+/// Default local DNS server address:
+pub const DEFAULT_DNS: &str = "172.16.3.1";
 
-    /// Default path to sentry metadata dirs:
-    pub const SENTRY_PATH: &str = "/Shared/Prison/Sentry";
+/// Default listen address to listen on:
+pub const DEFAULT_ADDRESS: &str = "172.16.3.1:80";
+
+/// Default path to Prison root dir:
+pub const PRISON_PATH: &str = "/Shared/Prison";
+
+/// Default path to cells data dirs:
+pub const CELLS_PATH: &str = "/Shared/Prison/Cells";
+
+/// Default path to sentry metadata dirs:
+pub const SENTRY_PATH: &str = "/Shared/Prison/Sentry";
 
 
-    /// WebAPI
-
-    /// Cell management:
-    pub const CELL_RESOURCE: &str = "/cell/";
-
-    /// Cell lists management:
-    pub const CELLS_RESOURCE: &str = "/cells/";
-
-    /// Igniter management:
-    pub const IGNITER_RESOURCE: &str = "/igniter/";
-
-    /// DNS zone management:
-    pub const ZONE_RESOURCE: &str = "/zone/";
-
-    /// Web proxy management:
-    pub const PROXY_RESOURCE: &str = "/proxy/";
-
-    /// Web proxies management:
-    pub const PROXIES_RESOURCE: &str = "/proxies/";
-
-    /// Cell status management:
-    pub const STATUS_RESOURCE: &str = "/status/";
-
-    /// Cell ZFS Snapshot management:
-    pub const SNAPSHOT_RESOURCE: &str = "/snapshot/";
-
-    /// Cell ZFS Rollback management:
-    pub const ROLLBACK_RESOURCE: &str = "/rollback/";
-
-    /// Cell ZFS datasets management:
-    pub const DATASETS_RESOURCE: &str = "/datasets/";
-
-}
+// EOF project constants.
 
 
 
+/// HTTP Request params static strings:
+
+/// Cell management:
+pub const CELL_RESOURCE: &str = "/cell/";
+
+/// Cell lists management:
+pub const CELLS_RESOURCE: &str = "/cells/";
+
+/// Igniter management:
+pub const IGNITER_RESOURCE: &str = "/igniter/";
+
+/// DNS zone management:
+pub const ZONE_RESOURCE: &str = "/zone/";
+
+/// Web proxy management:
+pub const PROXY_RESOURCE: &str = "/proxy/";
+
+/// Web proxies management:
+pub const PROXIES_RESOURCE: &str = "/proxies/";
+
+/// Cell status management:
+pub const STATUS_RESOURCE: &str = "/status/";
+
+/// Cell ZFS Snapshot management:
+pub const SNAPSHOT_RESOURCE: &str = "/snapshot/";
+
+/// Cell ZFS Rollback management:
+pub const ROLLBACK_RESOURCE: &str = "/rollback/";
+
+/// Cell ZFS datasets management:
+pub const DATASETS_RESOURCE: &str = "/datasets/";
 
 
 
+//
+// Public modules:
+//
 
 
+/// Public helpers, functions used by other modules:
+pub mod helpers;
 
+/// Public api modules used to "talk" with underlying system:
+pub mod apis;
 
+/// Web processors to handle WebAPI calls over HTTP:
+pub mod processors;
 
-
-
+/// Main router for Web processors:
+pub mod webrouter;
 
