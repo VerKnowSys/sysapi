517f4b14ad3fd345f8b0e4e257a1db9aa39ed7d3
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-21 19:22:10 +0100

    imp: Final form of the print_header() formatting and info.

--- src/helpers.rs
+++ src/helpers.rs
@@ -24,19 +24,20 @@ pub fn listen_address() -> String {
 
 /// Prints message pack on SysAPI service start:
 pub fn print_header() {
-    let version = env!("CARGO_PKG_VERSION");
-    info!("_______________________________________________________________________________________________________");
-    info!("SysAPI {} - design, implementation: {}.", format!("v{}", version).cyan(), CREATED_BY.cyan());
-    info!("               - in active development since 2011…");
-    info!("  This project is only a component of the '{}'.", "ServeD-OS project".cyan());
-    info!("               - a {}-driven, modern but production-quality operating-system.", "HardenedBSD".cyan());
-    info!("  Other projects:");
-    info!("               - {} -> {}", "sysapi".cyan(), "https://github.com/VerKnowSys/sysapi".green());
-    info!("               - {} -> {}", "svdOS".cyan(), "https://github.com/VerKnowSys/svdOS".green());
-    info!("               - {} -> {}", "Sofin".cyan(), "https://github.com/VerKnowSys/sofin".green());
-    info!("               - {} -> {}", "Sofin-definitions".cyan(), "https://github.com/VerKnowSys/sofin-definitions".green());
-    info!("               - {} -> {}", "kvmpro".cyan(), "https://github.com/VerKnowSys/kvmpro".green());
-    info!("               - {} -> {}", "Shable".cyan(), "https://github.com/VerKnowSys/Shable".green());
+    let version = format!("v{}", env!("CARGO_PKG_VERSION"));
+    info!("_________________________________________________________________________________________________");
+    info!("SysAPI {} - design, implementation: {} - licensed under: {}.", version.cyan(), CREATED_BY.cyan(), "MIT/BSD".cyan());
+    info!("");
+    info!("  This project is only a component of the '{}':", "ServeD-OS project".cyan());
+    info!("  {}-driven, modern, production-quality operating-system, developed since 2011.", "HardenedBSD".cyan());
+    info!("");
+    info!("  Other projects used by the system:");
+    info!("    - {} -> {}", "sysapi".cyan(), "https://github.com/VerKnowSys/sysapi".green());
+    info!("    - {} -> {}", "svdOS".cyan(), "https://github.com/VerKnowSys/svdOS".green());
+    info!("    - {} -> {}", "Sofin".cyan(), "https://github.com/VerKnowSys/sofin".green());
+    info!("    - {} -> {}", "Sofin-definitions".cyan(), "https://github.com/VerKnowSys/sofin-definitions".green());
+    info!("    - {} -> {}", "kvmpro".cyan(), "https://github.com/VerKnowSys/kvmpro".green());
+    info!("    - {} -> {}", "Shable".cyan(), "https://github.com/VerKnowSys/Shable".green());
     info!("");
     info!("SysAPI: ControlPane URL: {}", format!("{}://{}", DEFAULT_CONTROLPANE_PROTOCOL, listen_address()).cyan());
 }
