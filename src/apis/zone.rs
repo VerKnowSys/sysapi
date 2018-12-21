aa9d9444d35ea59efdc97f46178a6969a11bd2b4
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-12-21 04:14:42 +0100

    imp: More ANSI colors in log msgs from zone module.

--- src/apis/zone.rs
+++ src/apis/zone.rs
@@ -3,6 +3,7 @@ use std::net::{IpAddr, Ipv4Addr};
 use abstract_ns::HostResolve;
 use ns_std_threaded::ThreadedResolver;
 use futures::future::*;
+use colored::Colorize;
 
 
 /// DNS Zone types:
@@ -51,11 +52,12 @@ impl Zone {
                                 valid_ipv4_to.is_unspecified(),
                                 valid_ipv4_to.is_multicast());
                             let err_msg = format!("validate_domain_addresses(): Validation failed for pair: {} -> {}. Validation details:\n\n{}\n{}\n",
-                                                  valid_ipv4_from, valid_ipv4_to, validate_state, validate_state2);
+                                                  valid_ipv4_from.to_string().green(), valid_ipv4_to.to_string().green(),
+                                                  validate_state.to_string().cyan(), validate_state2.to_string().cyan());
                             error!("{}", err_msg);
                             Err(FromStrError::EmptyLabel)
                         } else {
-                            debug!("validate_domain_addresses(): IPv4 pair: {} -> {}", valid_ipv4_from, valid_ipv4_to);
+                            debug!("validate_domain_addresses(): IPv4 pair: {} -> {}", valid_ipv4_from.to_string().green(), valid_ipv4_to.to_string().green());
                             Ok((valid_ipv4_from, valid_ipv4_to))
                         }
                     })
@@ -69,7 +71,7 @@ impl Zone {
         let response = ThreadedResolver::new()
             .resolve_host(&domain.parse().unwrap())
             .map(|addresses| {
-                debug!("Domain: {} resolves to IPv4(s): [{:?}]", &domain, &addresses);
+                debug!("Domain: {} resolves to IPv4(s): [{}]", &domain.green(), &addresses.iter().map(|e| e.to_string()).collect::<String>().green());
                 addresses
                     .iter()
                     .filter(|&element| !element.to_string().contains(":")) /* NOTE: filter out IPv6 - unsupported yet */
@@ -86,7 +88,7 @@ impl Zone {
             })
             .unwrap_or(Some(ip_localhost));
 
-        info!("Domain: {} resolves to IPv4: {}", domain, resolved_ip.unwrap());
+        info!("Domain: {} resolves to IPv4: {}", domain.green(), resolved_ip.unwrap_or(ip_localhost).to_string().cyan());
         Ok(resolved_ip.unwrap())
     }
 
